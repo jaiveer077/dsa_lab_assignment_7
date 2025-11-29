da57c43a2712f76e151e9f1f97352c2597ce04ce
jaiveer077 <jaiveersagoo@gmail.com>
2025-11-29 23:26:24 +0530

    merge sort

---
+++ sorting/merge_sort.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void mergeArr(vector<int>&a,int l,int m,int r){
+    int n1=m-l+1,n2=r-m;
+    vector<int>L(n1),R(n2);
+    for(int i=0;i<n1;i++)L[i]=a[l+i];
+    for(int i=0;i<n2;i++)R[i]=a[m+1+i];
+    int i=0,j=0,k=l;
+    while(i<n1&&j<n2){
+        if(L[i]<=R[j])a[k++]=L[i++];
+        else a[k++]=R[j++];
+    }
+    while(i<n1)a[k++]=L[i++];
+    while(j<n2)a[k++]=R[j++];
+}
+void mergeSort(vector<int>&a,int l,int r){
+    if(l>=r)return;
+    int m=l+(r-l)/2;
+    mergeSort(a,l,m);
+    mergeSort(a,m+1,r);
+    mergeArr(a,l,m,r);
+}
+int main(){
+    vector<int>a={5,2,4,6,1,3};
+    mergeSort(a,0,a.size()-1);
+    for(int x:a)cout<<x<<" ";
+}
